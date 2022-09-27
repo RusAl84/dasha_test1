cab2f88097d7f8cb07a96022be362ea4d7fa1fd5
Aleksey <rusal@bk.ru>
2022-09-27 20:28:56 +0300

    Добавьте файлы проекта.

---
+++ Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dasha_test1
+{
+    internal class Message
+    {
+        public Message()
+        {
+            UserName = "Default";
+            MessageText = "Hello";
+            TimeStamp = "12:30";
+            Age = 18;
+            nums2 = new int[4];
+        }
+
+        public string UserName { get; set; }
+        public string MessageText { get; set; }
+        public string TimeStamp { get; set; }
+        public int Age { get; set; }
+
+        public int[] nums2  { get; set; }
+        public override string ToString()
+        {
+            string output = String.Format("{0} <{2}>: {1}", UserName, MessageText, TimeStamp);
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace dasha_test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Message firstMessage = new Message();
+
+            Console.WriteLine(firstMessage);
+            string json_string = JsonConvert.SerializeObject(firstMessage);
+            Console.WriteLine(json_string);
+
+            Message secondMessage = JsonConvert.DeserializeObject<Message>(json_string);
+            Console.WriteLine(secondMessage);
+
+        }
+    }
+}
